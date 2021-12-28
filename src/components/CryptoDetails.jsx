6c0023a22e4109bb2c09c3abf1c7dc296745d484
Napong Dungduangsasitorn <jamesnapongd@gmail.com>
2021-12-28 15:43:09 +0700

    Show line chart

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -35,7 +35,7 @@ const CryptoDetails = () => {
 
   if (isFetching) return 'Loading...';
 
-  const time = ['3h', '24h', '7d', '30d', '1y', '3m', '3y', '5y'];
+  const time = ['24h', '7d', '30d', '1y', '5y'];
 
   const stats = [
     {
